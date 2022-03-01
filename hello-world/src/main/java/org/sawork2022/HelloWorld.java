ea5dc864c085bd2769d9eeb1a31570662db06bd7
AlwaysSmiling2000 <1196168404@qq.com>
2022-03-01 19:16:54 +0000

    修复了idea中的项目文件信息

--- hello-world/src/main/java/org/sawork2022/HelloWorld.java
+++ hello-world/src/main/java/org/sawork2022/HelloWorld.java
@@ -4,6 +4,11 @@ import org.sawork2022.ascii.AsciiConsole;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/*
+    hello-world 模块基于 ascii-panel 项目完成了一个简单的，可以进行输入输出的
+    控制台。resources 目录下的 base-config.xml 文件是关于 spring 构件装配的
+    配置文件。
+ */
 public class HelloWorld {
     public static void main(String [] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("base-config.xml");
